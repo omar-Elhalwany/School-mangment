933ac14a15ccf89f9ed16bbb462b362c09a24a40
omar-Elhalwany <124082149+omar-Elhalwany@users.noreply.github.com>
2023-02-15 20:44:54 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAtdgGtbNTsh9EFBYDR8NJhrn1_kyMOwvs",
+  authDomain: "school-mangement-c3dd7.firebaseapp.com",
+  projectId: "school-mangement-c3dd7",
+  storageBucket: "school-mangement-c3dd7.appspot.com",
+  messagingSenderId: "1076194626877",
+  appId: "1:1076194626877:web:cbc8685b6df0fe87cdc954",
+  measurementId: "G-R8CX3K432V"
+};
+
+
+// Persons
+// accounts
+
+
+// rulls if true
